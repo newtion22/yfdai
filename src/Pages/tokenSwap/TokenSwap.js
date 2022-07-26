e837278f13a3a5cda15fa63927b430fc9c81b885
oforduhh <oforduhh@gmail.com>
2022-07-26 13:55:36 +0100

    last

--- src/Pages/tokenSwap/TokenSwap.js
+++ src/Pages/tokenSwap/TokenSwap.js
@@ -158,7 +158,7 @@ const TokenSwap = () => {
     return false;
   });
 
-  console.log(fArray);
+  // console.log(fArray);
   // console.log(result);
   // console.log(`This is the balance ${userYfdaiBal}`);
   // setStakeValue(userYfdaiBal);
